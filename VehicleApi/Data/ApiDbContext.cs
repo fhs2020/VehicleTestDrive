cebcd1465a55e5f4b26e36e2b4fda71f57ebb153
fhs2020 <flavioh007@gmail.com>
2022-01-25 08:52:21 -0300

    Add project files.

---
+++ VehicleApi/Data/ApiDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using VehicleApi.Models;
+
+namespace VehicleApi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public DbSet<Vehicle> Vehicles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=VehicleApiDb;");
+        }
+    }
+}
---
+++ VehicleApi/Interfaces/IVehicle.cs
@@ -0,0 +1,17 @@
+﻿using VehicleApi.Models;
+
+namespace VehicleApi.Interfaces
+{
+    public interface IVehicle
+    {
+        Task<List<Vehicle>> GetAllVehicle();
+
+        Task<Vehicle> GetVehicleById(int id);
+
+        Task AddVehicle(Vehicle vehicle);
+
+        Task UpdateVehicle(int id, Vehicle vehicle);
+
+        Task DeleteVehicle(int id);
+    }
+}
