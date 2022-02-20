3b24afdb0d706460a412a7677fb6f3220ae467c0
fhs2020 <flavioh007@gmail.com>
2022-02-20 11:46:05 -0300

    Adding connection for Azure Service Bus

--- CustomerApi/Services/CustomerServices.cs
+++ CustomerApi/Services/CustomerServices.cs
@@ -1,8 +1,12 @@
-﻿using CustomerApi.Data;
+﻿using Azure.Messaging.ServiceBus;
+
+using CustomerApi.Data;
 using CustomerApi.Interfaces;
 using CustomerApi.Model;
 
 using Microsoft.EntityFrameworkCore;
+
+using Newtonsoft.Json;
 
 namespace CustomerApi.Services
 {
@@ -27,6 +31,22 @@ namespace CustomerApi.Services
             customer.Vehicle = null;
             await dbContext.Customers.AddAsync(customer);
             await dbContext.SaveChangesAsync();
+
+            string connectionString = "Endpoint=sb://vehicletestdriveapp.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=nuIGL3F+LtXmOa6TZvMFx+HGEzEZsXUH9zo6bdD8xT8=";
+            string queueName = "azureorderqueue";
+            // since ServiceBusClient implements IAsyncDisposable we create it with "await using"
+            await using var client = new ServiceBusClient(connectionString);
+
+            var customerObjectAsText = JsonConvert.SerializeObject(customer);
+
+            // create the sender
+            ServiceBusSender sender = client.CreateSender(queueName);
+
+            // create a message that we can send. UTF-8 encoding is used when providing a string.
+            ServiceBusMessage message = new ServiceBusMessage(customerObjectAsText);
+
+            // send the message
+            await sender.SendMessageAsync(message);
         }
 
         public Task DeleteCustomer(int id)
--- VehicleApi/Program.cs
+++ VehicleApi/Program.cs
@@ -8,8 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options => { 
+        options.CustomSchemaIds(type => type.ToString());
+    });
 builder.Services.AddScoped<IVehicle, VehicleService>();
+
 
 var app = builder.Build();
 
